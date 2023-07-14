1925bc6f243ecf69fca62ecddb86e7e75a80f11f
devHyeon99 <jinung5@gmail.com>
2023-07-14 19:33:58 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,174 @@
+'use strict'
+
+/**
+ * 검색창 제어
+ */
+// 검색창 요소(.search) 찾기.
+const searchEl = document.querySelector('.search')
+const searchInputEl = searchEl.querySelector('input')
+// 검색창 요소를 클릭하면 실행.
+searchEl.addEventListener('click', function () {
+    searchInputEl.focus()
+})
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+searchInputEl.addEventListener('focus', function () {
+    searchEl.classList.add('focused')
+    searchInputEl.setAttribute('placeholder', '통합검색')
+})
+// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+searchInputEl.addEventListener('blur', function () {
+    searchEl.classList.remove('focused')
+    searchInputEl.setAttribute('placeholder', '')
+})
+
+
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+    // 페이지 스크롤 위치가 500px이 넘으면.
+    if (window.scrollY > 500) {
+        // Badge 요소 숨기기!
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        })
+        // 상단으로 스크롤 버튼 보이기!
+        gsap.to(toTopEl, .2, {
+            x: 0
+        })
+
+        // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    } else {
+        // Badge 요소 보이기!
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        })
+        // 상단으로 스크롤 버튼 숨기기!
+        gsap.to(toTopEl, .2, {
+            x: 100
+        })
+    }
+}, 300));
+toTopEl.addEventListener('click', function(){ // 해당 버튼을 클릭했을 경우 동작시킬 함수를 작성한다.
+	gsap.to(window, 0.7, {	// gsap의 사용방법
+    	scrollTo: 0		// 이 옵션을 사용하기 위해 ScrollToPlugin을 추가한 것. 
+    });
+});
+
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+    // 각 요소들을 순서대로(delay) 보여지게 함!
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    })
+})
+
+
+/**
+ * 슬라이드 요소 관리
+ */
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical', // 수직 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true // 반복 재생 여부
+})
+new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: { // 자동 재생 여부
+        delay: 5000 // 5초마다 슬라이드 바뀜
+    },
+    loop: true, // 반복 재생 여부
+    slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    pagination: { // 페이지 번호 사용 여부
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+        prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+        nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+    }
+})
+new Swiper('.awards .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true, // 반복 재생 여부
+    spaceBetween: 30, // 슬라이드 사이 여백
+    slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+    // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+        prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+        nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+    }
+})
+
+/**
+ * Promotion 슬라이드 토글 기능
+ */
+// 슬라이드 영역 요소 검색!
+const promotionEl = document.querySelector('.promotion')
+// 슬라이드 영역를 토글하는 버튼 검색!
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+// 슬라이드 영역 숨김 여부 기본값!
+let isHidePromotion = false
+// 토글 버튼을 클릭하면,
+promotionToggleBtn.addEventListener('click', function () {
+    // 슬라이드 영역 숨김 여부를 반댓값으로 할당!
+    isHidePromotion = !isHidePromotion
+    // 요소를 숨겨야 하면,
+    if (isHidePromotion) {
+        promotionEl.classList.add('hide')
+        // 요소가 보여야 하면,
+    } else {
+        promotionEl.classList.remove('hide')
+    }
+})
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+    gsap.to(selector, random(1.5, 2.5), {
+        y: size, // y축으로 얼마나 움직일거냐?
+        repeat: -1,
+        yoyo: true, // 한번 재생한 애니메이션을 다시 위로 올라가도록
+        ease: Power1.easeInOut, // ease 함수를 이용하여 애니메이션의 동작을 원하는대로 제어 가능함 부드럽게 가능
+        delay: random(0, delay)
+    })
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+            triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear()
